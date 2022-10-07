fab7e6934f7236ba1d1d0d94ebea6c029742ec0a
Alkhodir, Jana (k20061055) <jana.alkhodir@kcl.ac.uk>
2022-10-07 12:56:43 +0100

    Add an exepetion error

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+
+    n = 2
+    i=0
+
+    if 0 < number_of_primes:
+
+        while i < number_of_primes:
+            prime = True
+            for j in range(2, n) :
+                if n % j == 0:
+                    prime = False
+
+            if prime:
+                list.append (n)
+                i += 1
+
+            n += 1
+    else: 
+        raise ValueError('Number of values must bigger that 0.')
+    
+
     return list
